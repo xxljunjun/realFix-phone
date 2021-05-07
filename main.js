1bb89c27ceabeefe91c9d4d3c2d86ab2072a5868
junjun <793117268@qq.com>
2021-05-08 00:27:03 +0800

    初始化

--- main.js
+++ main.js
@@ -6,12 +6,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 Vue.prototype.$store = store
-Vue.prototype.$backgroundAudioData = {
-	playing: false,
-	playTime: 0,
-	formatedPlayTime: '00:00:00'
-}
-Vue.prototype.$adpid = "1111111111"
+
 
 App.mpType = 'app'
 
