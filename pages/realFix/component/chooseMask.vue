4061795882e45f7123056a3deae2d0d25a707e7f
xxljunjun <liaojunjun@autel.com>
2021-05-14 11:32:07 +0800

    选择车型

--- pages/realFix/component/chooseMask.vue
+++ pages/realFix/component/chooseMask.vue
@@ -8,7 +8,39 @@
           <image src="/static/daotong/close.png" class="close" @tap="toClose" />
         </view>
         <!-- 中间 -->
-        <view class="system_main"> </view>
+        <view class="system_main">
+          <view class="delBox">
+            <view class="title">The Makes I follow</view>
+            <view class="edit" @tap="toEdit">{{ status }}</view>
+          </view>
+          <!-- 已选择 -->
+          <view class="box_1">
+            <view class="itemBox" v-for="item in maks2Arr" :key="item.id"
+              >{{ item.maks2Name }}
+              <image
+                src="/static/daotong/delete.png"
+                class="close"
+                v-if="isToEdit"
+                @click="toDelMake(item.id)"
+              />
+            </view>
+          </view>
+          <view class="makeBox">
+            <view class="title">More Makes</view>
+          </view>
+          <!-- 未选择 -->
+          <view class="box_2">
+            <view class="itemBox" v-for="item in maks1Arr" :key="item.id"
+              >{{ item.maks1Name }}
+              <image
+                src="/static/daotong/jia.png"
+                class="jia"
+                v-if="isToEdit"
+                @click="toAddMake(item.id)"
+              />
+            </view>
+          </view>
+        </view>
       </view>
     </view>
   </view>
@@ -18,7 +50,23 @@
 export default {
   data() {
     return {
+      isToEdit: false,
+      status: 'Edit',
       advice: '',
+      maks1Arr: [
+        { id: 1, maks1Name: 'GM' },
+        { id: 2, maks1Name: 'BMW' },
+        { id: 3, maks1Name: 'Benz' },
+        { id: 4, maks1Name: 'Ford' },
+        { id: 5, maks1Name: 'Toyota' },
+      ],
+      maks2Arr: [
+        { id: 6, maks2Name: 'Jaguar' },
+        { id: 7, maks2Name: 'Mazda' },
+        { id: 8, maks2Name: 'Land Rover' },
+        { id: 9, maks2Name: 'Aston Martin' },
+        { id: 10, maks2Name: 'CHEVRDLET' },
+      ],
     }
   },
   props: {
@@ -31,8 +79,35 @@ export default {
   components: {},
   mounted() {},
   methods: {
+    toAddMake(id) {
+      console.log('增加')
+      this.maks1Arr = this.maks1Arr.filter((val) => {
+        if (val.id == id) {
+          this.maks2Arr.push({ id: id, maks2Name: val.maks1Name })
+        }
+        return val.id !== id
+      })
+    },
+    toDelMake(id) {
+      console.log('删除')
+      this.maks2Arr = this.maks2Arr.filter((val) => {
+        if (val.id == id) {
+          this.maks1Arr.push({ id: id, maks1Name: val.maks2Name })
+        }
+        return val.id !== id
+      })
+    },
     toClose() {
       this.$emit('toClose')
+    },
+    toEdit() {
+      if (this.status === 'Edit') {
+        this.status = 'Done'
+        this.isToEdit = true
+      } else {
+        this.status = 'Edit'
+        this.isToEdit = false
+      }
     },
   },
 }
@@ -95,53 +170,83 @@ export default {
       }
       .system_main {
         width: 750rpx;
-        height: 790rpx;
+        height: 1075rpx;
         background: rgba(235, 235, 235, 1);
-        padding-left: 30rpx;
+        padding: 0 0 0 30rpx;
+        box-sizing: border-box;
         overflow: auto;
-        .textmore {
-          margin-top: 30rpx;
+        .delBox {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          height: 100rpx;
           font-size: 34rpx;
-          font-family: HelveticaNeue;
-          color: rgba(0, 0, 0, 1);
-          line-height: 42rpx;
-          font-size: 30rpx;
-          .advicetextarea {
-            max-width: 690rpx;
-            height: 240rpx;
-            background: #fff;
-            box-shadow: 0px 4rpx 2rpx 0px rgba(226, 226, 226, 1);
-            border-radius: 10rpx;
-            border: 2rpx solid rgba(184, 184, 184, 1);
+          line-height: 41rpx;
+          .title {
+            color: #000;
+            font-weight: bolder;
+          }
+          .edit {
+            color: #007aff;
             font-size: 30rpx;
+            margin-right: 30rpx;
           }
         }
-        .selected {
-          background: rgba(206, 221, 237, 1);
-          color: #007eff;
+        .makeBox {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          height: 100rpx;
+          font-size: 34rpx;
+          line-height: 41rpx;
+          color: #000;
+          font-weight: bolder;
         }
-        .noselcted {
-          color: #555555;
-          background: rgba(223, 223, 223, 1);
+        .box_1 {
+          display: flex;
+          flex-wrap: wrap;
+          font-size: 30rpx;
+          .itemBox {
+            margin-right: 20rpx;
+            width: 335rpx;
+            height: 88rpx;
+            background: rgba(223, 223, 223, 1);
+            border-radius: 10rpx;
+            margin-bottom: 20rpx;
+            text-align: center;
+            line-height: 88rpx;
+            position: relative;
+            .close {
+              height: 30rpx;
+              width: 30rpx;
+              position: absolute;
+              right: 10rpx;
+              top: 10rpx;
+            }
+          }
         }
-        .item0 {
-          height: 72rpx;
-          min-width: 168rpx;
-          max-width: 540rpx;
-          text-align: center;
-          line-height: 72rpx;
-          display: inline-block;
-          margin-right: 20rpx;
-          margin-top: 20rpx;
-          padding: 0 35rpx;
-          border-radius: 10rpx;
-          position: relative;
-          .blue {
-            height: 34rpx;
-            width: 34rpx;
-            position: absolute;
-            bottom: 0;
-            right: 0;
+        .box_2 {
+          display: flex;
+          flex-wrap: wrap;
+          font-size: 30rpx;
+          .itemBox {
+            margin-right: 20rpx;
+            width: 335rpx;
+            height: 88rpx;
+            margin-bottom: 20rpx;
+            text-align: center;
+            line-height: 88rpx;
+            background: rgba(245, 245, 245, 1);
+            border-radius: 10rpx;
+            border: 1px solid rgba(190, 190, 190, 1);
+            position: relative;
+            .jia {
+              height: 30rpx;
+              width: 30rpx;
+              position: absolute;
+              right: 10rpx;
+              top: 10rpx;
+            }
           }
         }
       }
